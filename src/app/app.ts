cec6d01edf29398871e45806f35cbaed0bc25a0c
Roberto Ninno <roberto.ninno@synclab.it>
2025-10-13 12:41:59 +0200

    added first component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PostCreate } from './posts/post-create/post-create';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PostCreate],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
+
 export class App {
   protected readonly title = signal('mean-course');
 }
