02fb26aad365196fd85e6fdaea4a6af3c8c1dbc8
Roberto Ninno <roberto.ninno@synclab.it>
2025-10-14 16:55:00 +0200

    updated postlist

--- src/app/posts/post-list/post-list.ts
+++ src/app/posts/post-list/post-list.ts
@@ -3,14 +3,16 @@ import { Subscription } from 'rxjs';
 import {
   MatAccordion,
   MatExpansionPanel,
-  MatExpansionPanelHeader
+  MatExpansionPanelHeader,
+  MatExpansionPanelActionRow
 } from "@angular/material/expansion";
+import { MatButtonModule } from '@angular/material/button';
 import { Post } from '../post.model';
 import { PostsService } from '../posts.service';
 
 @Component({
   selector: 'app-post-list',
-  imports: [MatAccordion, MatExpansionPanel, MatExpansionPanelHeader],
+  imports: [MatAccordion, MatExpansionPanel, MatExpansionPanelHeader, MatExpansionPanelActionRow, MatButtonModule],
   templateUrl: './post-list.html',
   styleUrls: ['./post-list.css']
 })
