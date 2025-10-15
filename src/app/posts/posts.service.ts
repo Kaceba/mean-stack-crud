220450751b7c54d436607d553cf1b9203e583c7c
Roberto Ninno <roberto.ninno@synclab.it>
2025-10-15 15:37:00 +0200

    added post method and typescript

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -10,9 +10,9 @@ export class PostsService {
   private posts: Post[] = [];
   private postsUpdated = new Subject<Post[]>();
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
-  getPosts(){
+  getPosts() {
     this.http.get<{ message: string; posts: Post[] }>('http://localhost:3000/api/posts')
       .subscribe((postData) => {
         this.posts = postData.posts;
@@ -26,7 +26,11 @@ export class PostsService {
 
   addPost(title: string, content: string): void {
     const post: Post = { id: null, title: title, content: content };
-    this.posts.push(post);
-    this.postsUpdated.next([...this.posts]);
+    this.http.post<{ message: string }>('http://localhost:3000/api/posts', post)
+      .subscribe((responseData) => {
+        console.log(responseData.message);
+        this.posts.push(post);
+        this.postsUpdated.next([...this.posts]);
+      });
   }
 }
