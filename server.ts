451b787eb1319a0aa19b1df6f5958631f52ea4b2
Roberto Ninno <roberto.ninno@synclab.it>
2025-10-16 14:49:21 +0200

    formatted server.ts

--- server.ts
+++ server.ts
@@ -1,50 +1,50 @@
-console.log("Server is starting...");
+console.log('Server is starting...');
 
 import app from './backend/app';
-import debug from "debug";
-import http from "http";
+import debug from 'debug';
+import http from 'http';
 
-const debugLog = debug("node-angular");
+const debugLog = debug('node-angular');
 
 const normalizePort = (val: string | number) => {
-    const port = typeof val === 'string' ? parseInt(val, 10) : val;
+  const port = typeof val === 'string' ? parseInt(val, 10) : val;
 
-    if (isNaN(port)) {
-        return val;
-    }
-    if (port >= 0) {
-        return port;
-    }
-    return false;
-}
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+};
 
 const onError = (error: NodeJS.ErrnoException) => {
-    if (error.syscall !== 'listen') {
-        throw error;
-    }
-    const bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+  const bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
 
-    switch (error.code) {
-        case 'EACCES':
-            console.error(bind + ' requires elevated privileges.');
-            process.exit(1);
-        case 'EADDRINUSE':
-            console.error(bind + ' is already in use.');
-            process.exit(1);
-        default:
-            throw error;
-    }
-}
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges.');
+      process.exit(1);
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use.');
+      process.exit(1);
+    default:
+      throw error;
+  }
+};
 
 const onListening = () => {
-    const addr = server.address();
-    if (!addr) {
-        console.error('Unable to get server address');
-        return;
-    }
-    const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
-    debugLog('Listening on ' + bind);
-}
+  const addr = server.address();
+  if (!addr) {
+    console.error('Unable to get server address');
+    return;
+  }
+  const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
+  debugLog('Listening on ' + bind);
+};
 
 const port = normalizePort(process.env['PORT'] || '3000');
 app.set('port', port);
@@ -54,4 +54,4 @@ server.on('error', onError);
 server.on('listening', onListening);
 server.listen(port);
 
-console.log("Started! Listening on port " + port);
+console.log('Started! Listening on port ' + port);
